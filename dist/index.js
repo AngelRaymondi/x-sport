38580b7636060b58ea5f5488ba8d14cc0127dff3
AngelDevelopment <raymondi.diaz.angel.aaron@gmail.com>
2023-05-05 22:30:50 -0500

    Pronósticos

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,34 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const x_sport_1 = __importDefault(require("./x-sport"));
+const x_sport_1 = __importStar(require("./x-sport"));
 const readline_1 = __importDefault(require("readline"));
+const cli_color_1 = __importDefault(require("cli-color"));
 const rl = readline_1.default.createInterface(process.stdin, process.stdout);
 const input = (text, filter = () => true) => {
     return new Promise((resolve) => {
@@ -19,19 +43,18 @@ const input = (text, filter = () => true) => {
         });
     });
 };
-(async () => {
+const analize = async () => {
     const url = await input("Introduce la url:", (answer) => answer.startsWith("https://www.flashscore.com/match/") &&
         answer.endsWith("/") &&
         answer.length ===
             "https://www.flashscore.com/match/bPgTL24A/".length);
-    console.log('Regla de filtrado\n\tEjm.: (x) => x >= 2\nEn el ejemplo, "x" es el número de puntos por tablero(home, away, overall)');
+    console.log('Regla de filtrado\n\tEjm.: 2.5\nEn el ejemplo, "2.5" es el número mínimo de puntos por tablero(home, away, overall)');
     const rule_filters = {
-        first: await input('5 Últimos local:'),
-        second: await input('5 Últimos visitante:'),
-        home: await input('Local:'),
-        away: await input('Visita:'),
+        first: parseFloat(await input("5 Últimos local:", (t) => !isNaN(parseFloat(t)))),
+        second: parseFloat(await input("5 Últimos visitante:", (t) => !isNaN(parseFloat(t)))),
+        home: parseFloat(await input("Local:", (t) => !isNaN(parseFloat(t)))),
+        away: parseFloat(await input("Visita:", (t) => !isNaN(parseFloat(t)))),
     };
-    rl.close();
     console.clear();
     const client = new x_sport_1.default();
     const match = await client.analize(url);
@@ -49,48 +72,9 @@ const input = (text, filter = () => true) => {
             return true;
         });
     };
-    const get_average = (array) => array.reduce((a, b) => a + b, 0) / array.length;
-    let first_total_points = [];
-    for (const m of match.overall.first) {
-        let total_points = 0;
-        for (const participant of m.participants) {
-            total_points += participant.points;
-        }
-        first_total_points.push(total_points);
-    }
-    let first_average_points = get_average(first_total_points);
-    let second_total_points = [];
-    for (const m of match.overall.second) {
-        let total_points = 0;
-        for (const participant of m.participants) {
-            total_points += participant.points;
-        }
-        second_total_points.push(total_points);
-    }
-    let second_average_points = get_average(second_total_points);
-    let home_total_points = [];
-    for (const m of match.home) {
-        let total_points = 0;
-        for (const participant of m.participants) {
-            total_points += participant.points;
-        }
-        home_total_points.push(total_points);
-    }
-    let home_average_points = get_average(home_total_points);
-    let away_total_points = [];
-    for (const m of match.away) {
-        let total_points = 0;
-        for (const participant of m.participants) {
-            total_points += participant.points;
-        }
-        away_total_points.push(total_points);
-    }
-    let away_average_points = get_average(away_total_points);
     console.log();
-    let first_filtered = first_total_points.filter(eval(rule_filters.first));
-    let second_filtered = second_total_points.filter(eval(rule_filters.second));
-    let home_filtered = home_total_points.filter(eval(rule_filters.home));
-    let away_filtered = away_total_points.filter(eval(rule_filters.away));
+    const { first_overall: first_average_points, second_overall: second_average_points, home: home_average_points, away: away_average_points, } = match.average_points;
+    const { first_overall: first_total_points, second_overall: second_total_points, home: home_total_points, away: away_total_points, } = match.total_points;
     console.log("PROMEDIO DE PUNTOS[NO FILTRADO]:");
     console.table({
         "5 Últimos local": first_average_points,
@@ -108,6 +92,10 @@ const input = (text, filter = () => true) => {
     });
     console.log();
     console.log(`FILTRADO:\nREGLA DE FILTRO:\n${JSON.stringify(rule_filters, null, 3)}`);
+    let first_filtered = first_total_points.filter((e) => e >= rule_filters.first);
+    let second_filtered = second_total_points.filter((e) => e >= rule_filters.second);
+    let home_filtered = home_total_points.filter((e) => e >= rule_filters.home);
+    let away_filtered = away_total_points.filter((e) => e >= rule_filters.away);
     console.table({
         "5 Últimos local": first_filtered,
         "5 Últimos visita": second_filtered,
@@ -123,35 +111,58 @@ const input = (text, filter = () => true) => {
         visita: both_score(match.away).length,
     });
     console.log();
-    console.log("Apostable (%)", ((first_filtered.length +
-        second_filtered.length +
-        home_filtered.length +
-        away_filtered.length) /
-        20) *
-        100 +
-        "%");
+    const quote = ((0, x_sport_1.get_average)(first_filtered) +
+        (0, x_sport_1.get_average)(second_filtered) +
+        (0, x_sport_1.get_average)(home_filtered) +
+        (0, x_sport_1.get_average)(away_filtered)) /
+        4;
+    // console.log(
+    //      "Apostable(Referente a los puntos con regla de filtro)",
+    //      quote.toFixed(2),
+    //      `=> Mayor o igual a`,
+    //      match.prognostic.calculate('high'), //https://www.flashscore.com/match/tYAmldPN/ da NaN
+    //      "puntos"
+    //      // ((first_filtered.length +
+    //      //      second_filtered.length +
+    //      //      home_filtered.length +
+    //      //      away_filtered.length) /
+    //      //      20) *
+    //      //      100 +
+    //      //      "%"
+    // );
+    console.log("\n".repeat(3));
+    console.log("OVERALL-FIRST");
+    console.table(match.overall.first.map((e) => ({
+        name: `${e.participants[0].name} - ${e.participants[1].name}`,
+        result: `${e.participants[0].points} - ${e.participants[1].points}`,
+    })));
+    console.log();
+    console.log("OVERALL-SECOND");
+    console.table(match.overall.second.map((e) => ({
+        name: `${e.participants[0].name} - ${e.participants[1].name}`,
+        result: `${e.participants[0].points} - ${e.participants[1].points}`,
+    })));
+    console.log();
+    console.log("HOME");
+    console.table(match.home.map((e) => ({
+        name: `${e.participants[0].name} - ${e.participants[1].name}`,
+        result: `${e.participants[0].points} - ${e.participants[1].points}`,
+    })));
+    console.log();
+    console.log("OVERALL-VISITA");
+    console.table(match.away.map((e) => ({
+        name: `${e.participants[0].name} - ${e.participants[1].name}`,
+        result: `${e.participants[0].points} - ${e.participants[1].points}`,
+    })));
     console.log('\n'.repeat(3));
-    console.log('OVERALL-FIRST');
-    console.table(match.overall.first.map(e => ({
-        name: `${e.participants[0].name} - ${e.participants[1].name}`,
-        result: `${e.participants[0].points} - ${e.participants[1].points}`
-    })));
-    console.log();
-    console.log('OVERALL-SECOND');
-    console.table(match.overall.second.map(e => ({
-        name: `${e.participants[0].name} - ${e.participants[1].name}`,
-        result: `${e.participants[0].points} - ${e.participants[1].points}`
-    })));
-    console.log();
-    console.log('HOME');
-    console.table(match.home.map(e => ({
-        name: `${e.participants[0].name} - ${e.participants[1].name}`,
-        result: `${e.participants[0].points} - ${e.participants[1].points}`
-    })));
-    console.log();
-    console.log('OVERALL-VISITA');
-    console.table(match.away.map(e => ({
-        name: `${e.participants[0].name} - ${e.participants[1].name}`,
-        result: `${e.participants[0].points} - ${e.participants[1].points}`
-    })));
-})();
+    console.log(cli_color_1.default.yellow('CONCLUSIONES: '));
+    console.log("Cantidad de puntos: ");
+    console.log("\nEn el mejor de los casos:", cli_color_1.default.yellow(`Mayor o igual a ${Math.round(parseFloat(quote.toFixed(2)))}`));
+    console.log(`En el caso normal: ${cli_color_1.default.yellow(`Mayor o igual a ${match.prognostic.calculate("high")}`)}`);
+    console.log(`Uno de los casos asegurados${cli_color_1.default.yellow('*')}: ${cli_color_1.default.yellow(`Mayor o igual a ${match.prognostic.calculate("low")}`)}`);
+    console.log('\n'.repeat(2));
+    await input("¿Analizar otro evento? Se eliminará el contenido (:y)", (e) => e === ":y");
+    console.clear();
+    return analize();
+};
+analize();
--- dist/x-sport.js
+++ dist/x-sport.js
@@ -3,7 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.get_average = void 0;
 const puppeteer_1 = __importDefault(require("puppeteer"));
+const get_average = (array) => array.reduce((a, b) => a + b, 0) / array.length || 0;
+exports.get_average = get_average;
 const selectors = {
     h2h: 'a[href="#/h2h"]',
     overall: [
@@ -19,20 +22,33 @@ class XSport {
         this.analize = async (match) => {
             const browser = await puppeteer_1.default.launch({
                 headless: false,
+                timeout: 5 * 1000 * 60
             });
             console.log("Browser launched!");
             const page = await browser.newPage();
             console.log("Entering website...");
             await page.goto(match);
             console.log("Going to overall...");
-            await page.waitForSelector(selectors.h2h);
+            await page.waitForSelector(selectors.h2h, {
+                timeout: 5 * 60 * 1000,
+            });
             await page.$eval(selectors.h2h, (e) => e.click());
-            await page.waitForNavigation();
+            await page.waitForNavigation({
+                timeout: 5 * 60 * 1000,
+            });
             console.log("Extracting first overall");
-            await page.waitForSelector(selectors.overall[0]);
-            await page.waitForSelector(selectors.overall[1]);
-            await page.waitForSelector(selectors.home);
-            await page.waitForSelector(selectors.away);
+            await page.waitForSelector(selectors.overall[0], {
+                timeout: 5 * 60 * 1000,
+            });
+            await page.waitForSelector(selectors.overall[1], {
+                timeout: 5 * 60 * 1000,
+            });
+            await page.waitForSelector(selectors.home, {
+                timeout: 5 * 60 * 1000,
+            });
+            await page.waitForSelector(selectors.away, {
+                timeout: 5 * 60 * 1000,
+            });
             const first_overall = await page.$(selectors.overall[0]);
             const second_overall = await page.$(selectors.overall[1]);
             const first_overall_matches = await first_overall.$$(".h2h__row");
@@ -63,17 +79,31 @@ class XSport {
                 const date = await match.$eval(".h2h__date", (el) => el.innerText);
                 const event = await match.$eval(".h2h__event .flag", (el) => el.getAttribute("title"));
                 const result = await match.$(".h2h__result");
+                let participant_1_points = null;
+                try {
+                    participant_1_points = await result.$eval("span:nth-child(1)", (el) => parseFloat(el.innerText));
+                }
+                catch {
+                    participant_1_points = null;
+                }
+                let participant_2_points = null;
+                try {
+                    participant_2_points = await result.$eval("span:nth-child(2)", (el) => parseFloat(el.innerText));
+                }
+                catch {
+                    participant_2_points = null;
+                }
                 const match_data = {
                     date,
                     event,
                     participants: [
                         {
                             name: await match.$eval(".h2h__participant:nth-child(3)", (el) => el.innerText.trim()),
-                            points: await result.$eval("span:nth-child(1)", (el) => parseFloat(el.innerText)),
+                            points: participant_1_points,
                         },
                         {
                             name: await match.$eval(".h2h__participant:nth-child(4)", (el) => el.innerText.trim()),
-                            points: await result.$eval("span:nth-child(2)", (el) => parseFloat(el.innerText)),
+                            points: participant_2_points,
                         },
                     ],
                 };
@@ -113,17 +143,31 @@ class XSport {
                 const date = await match.$eval(".h2h__date", (el) => el.innerText);
                 const event = await match.$eval(".h2h__event .flag", (el) => el.getAttribute("title"));
                 const result = await match.$(".h2h__result");
+                let participant_1_points = null;
+                try {
+                    participant_1_points = await result.$eval("span:nth-child(1)", (el) => parseFloat(el.innerText));
+                }
+                catch {
+                    participant_1_points = null;
+                }
+                let participant_2_points = null;
+                try {
+                    participant_2_points = await result.$eval("span:nth-child(2)", (el) => parseFloat(el.innerText));
+                }
+                catch {
+                    participant_2_points = null;
+                }
                 const match_data = {
                     date,
                     event,
                     participants: [
                         {
                             name: await match.$eval(".h2h__participant:nth-child(3)", (el) => el.innerText.trim()),
-                            points: await result.$eval("span:nth-child(1)", (el) => parseFloat(el.innerText)),
+                            points: participant_1_points,
                         },
                         {
                             name: await match.$eval(".h2h__participant:nth-child(4)", (el) => el.innerText.trim()),
-                            points: await result.$eval("span:nth-child(2)", (el) => parseFloat(el.innerText)),
+                            points: participant_2_points,
                         },
                     ],
                 };
@@ -131,6 +175,64 @@ class XSport {
             }
             console.log("Finished!");
             await browser.close();
+            let first_total_points = [];
+            for (const m of first_overall_data) {
+                let total_points = 0;
+                for (const participant of m.participants) {
+                    total_points += participant.points;
+                }
+                first_total_points.push(total_points);
+            }
+            let first_average_points = (0, exports.get_average)(first_total_points);
+            let second_total_points = [];
+            for (const m of second_overall_data) {
+                let total_points = 0;
+                for (const participant of m.participants) {
+                    total_points += participant.points;
+                }
+                second_total_points.push(total_points);
+            }
+            let second_average_points = (0, exports.get_average)(second_total_points);
+            let home_total_points = [];
+            for (const m of home_data) {
+                let total_points = 0;
+                for (const participant of m.participants) {
+                    total_points += participant.points;
+                }
+                home_total_points.push(total_points);
+            }
+            let home_average_points = (0, exports.get_average)(home_total_points);
+            let away_total_points = [];
+            for (const m of away_data) {
+                let total_points = 0;
+                for (const participant of m.participants) {
+                    total_points += participant.points;
+                }
+                away_total_points.push(total_points);
+            }
+            let away_average_points = (0, exports.get_average)(away_total_points);
+            let prognostic = (accuracy) => {
+                let rule_filter = 0;
+                if (accuracy === "high") {
+                    rule_filter = 2.5;
+                }
+                else if (accuracy === "low") {
+                    rule_filter = 0;
+                }
+                else {
+                    rule_filter = accuracy;
+                }
+                let first_filtered = first_total_points.filter((e) => e >= rule_filter);
+                let second_filtered = second_total_points.filter((e) => e >= rule_filter);
+                let home_filtered = home_total_points.filter((e) => e >= rule_filter);
+                let away_filtered = away_total_points.filter((e) => e >= rule_filter);
+                const quote = ((0, exports.get_average)(first_filtered) +
+                    (0, exports.get_average)(second_filtered) +
+                    (0, exports.get_average)(home_filtered) +
+                    (0, exports.get_average)(away_filtered)) /
+                    4;
+                return Math.trunc(Math.round(parseFloat(quote.toFixed(2))) / 2);
+            };
             return {
                 overall: {
                     first: first_overall_data,
@@ -138,6 +240,21 @@ class XSport {
                 },
                 home: home_data,
                 away: away_data,
+                prognostic: {
+                    calculate: prognostic,
+                },
+                average_points: {
+                    first_overall: first_average_points,
+                    second_overall: second_average_points,
+                    home: home_average_points,
+                    away: away_average_points,
+                },
+                total_points: {
+                    first_overall: first_total_points,
+                    second_overall: second_total_points,
+                    home: home_total_points,
+                    away: away_total_points,
+                },
             };
             //1    -    8x                MÁS RECIENTE
             //2    -    7x
